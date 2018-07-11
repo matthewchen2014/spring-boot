944e271b59e27f7af6390f0a42e503c48746f0e4
matthew <matthewchen2014@hotmail.com>
2018-07-11 23:16:26 +0800

    remove unused annotation

--- src/main/java/com/spring/model/UserExtInfo.java
+++ src/main/java/com/spring/model/UserExtInfo.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
-import org.hibernate.annotations.Generated;
 
 import javax.persistence.*;
 
